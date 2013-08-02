f089554daef0c7a9dfea7295effe4eac1bb14920
Victor Safronovich <vsafronovich@gmail.com>
2013-08-02 09:02:09 +0600

    update part criteries code

--- tecdoc/models/criteria.py
+++ tecdoc/models/criteria.py
@@ -17,13 +17,24 @@ class Criteria(TecdocModel):
     short_designation = models.ForeignKey('tecdoc.Designation',
                                           verbose_name=u'Краткое Обозначение',
                                           db_column='CRI_SHORT_DES_ID',
-                                          related_name='+')
+                                          related_name='+',
+                                          blank=True, null=True)
 
     unit = models.ForeignKey('tecdoc.Designation',
                              verbose_name=u'Упаковка',
                              db_column='CRI_UNIT_DES_ID',
-                             related_name='+')
+                             related_name='+',
+                             blank=True, null=True)
 
+    '''
+    A – (почти аналогичен N) критерий текстовый, однако он содержится в VALUE (ARTICLE_CRITERIA=>ACR_VALUE)
+    B – информационный номер
+    D – годы выпуска (почти аналогичен N) критерий текстовый, однако он содержится в VALUE (ARTICLE_CRITERIA=>ACR_VALUE)
+    K – критерий текстовый, значение содержится в DES_ID (ARTICLE_CRITERIA=>ACR_KV_DES_ID),
+        также для этих критериев нужно рассматривать значение KV_KT_ID
+    N – критерий номерной, значение содержится в VALUE (ARTICLE_CRITERIA=>ACR_VALUE)
+    V – критерий сам является значением. Поля DES_ID или VALUE пустые.
+    '''
     type = models.CharField(u'Тип', max_length=1,
                             db_column='CRI_TYPE')
 
@@ -40,7 +51,7 @@ class Criteria(TecdocModel):
         db_table = tdsettings.DB_PREFIX + 'CRITERIA'
 
     def __unicode__(self):
-        return u'%s %s %s %s' % (self.type, 
+        return u'%s %s %s %s' % (self.type,
                                  self.designation or self.short_designation,
                                  self.get_unit(),
                                  self.is_interval and u'Интервальный' or '')
@@ -48,10 +59,15 @@ class Criteria(TecdocModel):
     def get_unit(self):
         return self.unit if self.unit_id else ''
 
+    def get_display_value(self):
+        return self.short_designation or self.designation
+
 
 class PartCriteria(TecdocModel):
+    # XXX not a primary key
     part = models.ForeignKey('tecdoc.Part', verbose_name=u'Запчасть',
-                             db_column='ACR_ART_ID')
+                             db_column='ACR_ART_ID', related_name='criteria_values',
+                             primary_key=True)
 
     group = models.ForeignKey('tecdoc.Group', verbose_name=u'Группа Запчастей',
                               db_column='ACR_GA_ID')
@@ -62,14 +78,27 @@ class PartCriteria(TecdocModel):
     value = models.CharField(u'Значение', max_length=60,
                              db_column='ACR_VALUE')
 
-    description = models.ForeignKey('tecdoc.Designation',
+    designation = models.ForeignKey('tecdoc.Designation',
                                     verbose_name=u'Описание',
-                                    db_column='ACR_KV_DES_ID')
+                                    db_column='ACR_KV_DES_ID',
+                                    blank=True, null=True)
 
     sorting = models.IntegerField(u'Порядок', db_column='ACR_SORT')
 
+    display = models.BooleanField(u'Показывать',
+                                  db_column='ACR_DISPLAY')
+
+    # XXX join remove rows with ACR_KV_DES_ID==0
+    objects = TecdocManagerWithDes()
+
     class Meta(TecdocModel.Meta):
         db_table = tdsettings.DB_PREFIX + 'ARTICLE_CRITERIA'
+        ordering = ['sorting']
+
+    def __unicode__(self):
+        return u'%s: %s' % (self.criteria.get_display_value(),
+                            self.get_value())
 
     def get_value(self):
-        return self.value or self.description
+        #criteria_type = self.criteria.type
+        return self.value or self.designation
